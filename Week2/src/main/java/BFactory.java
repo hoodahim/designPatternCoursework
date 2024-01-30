98d828c560cbeb1f2a17244a1d5adf0d47b8d753
hoodahim <himanshu243639@gmail.com>
2024-01-30 16:29:30 +0200

    Assignment2 -Concrete factory created without passing parameter

---
+++ Week2/src/main/java/BFactory.java
@@ -0,0 +1,17 @@
+public class BFactory extends UIFactory{
+
+    @Override
+    public Button createButton(String text) {
+        return new ButtonB();
+    }
+
+    @Override
+    public TextField createTextField(String text) {
+        return new TextFieldB();
+    }
+
+    @Override
+    public Checkbox createCheckbox(String text) {
+        return new CheckboxB();
+    }
+}
