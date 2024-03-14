5b2edf9b976a3702377206361b756b0677f09e44
Himanshu <himanshu243639@gmail.com>
2024-03-15 01:47:26 +0200

    ChainOfResponsibility- Complete

---
+++ ChainOfResponsibility/src/DevelopmentSuggestions.java
@@ -0,0 +1,16 @@
+public class DevelopmentSuggestions extends Feedback{
+
+    @Override
+    public void process(Message message){
+        if(message.messageType == MessageType.DEVELOPMENT_SUGGESTIONS){
+            System.out.println("Your Suggestion: " + message.content + " has been acknowledged!");
+            System.out.println("You can participate in promo campaign by clicking the code sent to: " + message.email);
+            System.out.println();
+        }
+        else {
+            // System response
+            System.out.println("This is not a Development Suggestion. Message is forwarded!");
+            this.getNextFeedback().process(message);
+        }
+    }
+}
---
+++ ChainOfResponsibility/src/Feedback.java
@@ -0,0 +1,19 @@
+public abstract class Feedback {
+
+    private Feedback nextFeedback;
+
+    public void process(Message message){
+        if(nextFeedback != null){
+            nextFeedback.process(message);
+        }
+    }
+
+    public void setNextFeedback(Feedback feedback){
+        this.nextFeedback = feedback;
+    }
+
+    public Feedback getNextFeedback(){
+        return nextFeedback;
+    }
+}
+
