c515104a1e9b7a5df3828743fcbd87aa299cee84
Himanshu <himanshu243639@gmail.com>
2024-03-25 20:03:24 +0200

    Memento- Existing undo functionality

---
+++ Memento_/src/main/java/Controller.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private Model model;
+    private Gui gui;
+    private List<IMemento> history; // Memento history
+    private List<IMemento> redo;
+
+    public Controller(Gui gui) {
+        this.model = new Model();
+        this.gui = gui;
+        this.history = new ArrayList<>();
+        this.redo = new ArrayList<>();
+    }
+
+    public void setOption(int optionNumber, int choice) {
+        saveToHistory();
+        model.setOption(optionNumber, choice);
+    }
+
+    public int getOption(int optionNumber) {
+        return model.getOption(optionNumber);
+    }
+
+    public void setIsSelected(boolean isSelected) {
+        saveToHistory();
+        model.setIsSelected(isSelected);
+    }
+
+    public boolean getIsSelected() {
+        return model.getIsSelected();
+    }
+
+    public void undo() {
+        if (!history.isEmpty()) {
+            System.out.println("Memento found in history");
+            IMemento previousState = history.remove(history.size() - 1);
+            redo.add(previousState);
+            model.restoreState(previousState);
+            gui.updateGui();
+        }
+    }
+
+    private void saveToHistory() {
+        IMemento currentState = model.createMemento();
+        history.add(currentState);
+    }
+
+    public void redo(){
+        if(!redo.isEmpty()){
+
+        }
+    }
+}
